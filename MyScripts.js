15fc8c1af5371aa9fbd0602b92f07cbe889210d2
MetatronL <paunbogdan97@windowslive.com>
2015-12-25 06:49:44 +0200

    v1.5

--- MyScripts.js
+++ MyScripts.js
@@ -4,13 +4,16 @@ var AutoLose =1;
 
 var square_width = 50 ;
 var _rows = 0;
-var_cols = 0;
+var _cols = 0;
 var current_diff = 0;
 
 var stare = [] ;
 var raport = [ 0.2 , 0.3 , 0.4 , 0.5 , 0.6 , 0.7  ];
 var bmb= [];
 var mat = [];
+var use = [];
+var cx = [];
+var cy = [];
 var nrBombe = 0; 
 
 window.oncontextmenu = function (){return false; // cancel default menu  
@@ -24,7 +27,7 @@ function createMatrix(mm)
 		mm[i] = [];
 		mm[i].length = _cols+2;
 		for(var j=0;j<_cols+2;++j)
-			m[i][j] = 0;
+			mm[i][j] = 0;
 	}
 	
 }
@@ -52,10 +55,12 @@ function Generate()
 	bmb.length = 0;
 	
 	stare.length = _rows * _cols +1;
+	cx.length = len+1;
+	cy.length = len+1;
 	
 	
 	createMatrix(mat) ;
-	
+	createMatrix(use) ;
 	
 	_LoadSquares(_rows,_cols);
 	
@@ -83,16 +88,14 @@ function Generate()
 	
 	for(var i=1;i<=_rows;++i)
 	{
-		x = (i-1)*_rows ;
+		x = (i-1)*_cols ;
 		for(var j=1;j<=_cols;++j)
 		{
 			++x;
-			mat[i][j] = stare[x];
+			mat[i][j] = -stare[x];
 			if( stare[x] == 1 ){
 				bmb.push( [i,j] );
 			}
-			
-		
 		}
 	}
 	
@@ -101,22 +104,26 @@ function Generate()
 	{
 		for(var j=1;j<=_cols;++j)
 		{
+			x = (i-1)*_cols + j;
+			cx[x]=i;
+			cy[x]=j;
+			
 			if( (mat[i][j] != 0) ) continue;
-			x = (i-1)*_rows + j;
-			for(var a=-1; a<2; ++a){
-				for(var b=-1;b<2;++b){
+			
+			for(var a=-1; a<2; ++a)
+			{
+				for(var b=-1;b<2;++b)
+				{
 					if( a==0 && b == 0) continue;
-					if( mat[i+a][j+b] == 1 ) ++mat[i][j];
+					if( mat[i+a][j+b] == -1 ) 
+						++mat[i][j];
 					
 				}
 			}
-			if(mat[i][j] >= 0 )
-			mat[i][j] = - mat[i][j];
 			
 		}
 	}
 	for(var i=1;i<=_rows;++i)
-	
 		for(var j=1;j<=_cols;++j)
 			document.getElementById("demo").innerHTML += "mat["+i+"]["+j+"] = "+mat[i][j]+"    ";
 	
@@ -128,28 +135,22 @@ function Generate()
 
 function mark_(_this)
 {
-	var id = _this.id;
-	id = id.replace("SquareDiv_","");
-	id = parseInt(id);
+	var nr = parseInt( _this.id.substring(10,_this.id.length) );
 	var x,y;
-	x= Math.floor( (id-1)/current_cols +1 ) ;
-	y= id%current_cols;
+	x= Math.floor( (nr-1)/_cols +1 ) ;
+	y= nr%_cols; 
+	if(y==0) y = _cols;
 	
-	if( mat[x][y] == 50 ){
-		_this.style.backgroundColor = "#2d65fb";
-		/*
-		_this.onmouseover="m_on(this)";
-		_this.onmouseout="m_out(this)";
-		_this.onclick=function clicked(this)";*/
-		mat[x][y]= 2;
-		stare[id] = 0;
-	}
-	else if(mat[x][y] <= 2 )
-	{
-		mat[x][y] = 50;
+	if( use[x][y] > 0 ) return;
+	
+	if( use[x][y] ==0){
 		_this.style.backgroundColor="#6f0038";
-		stare[id] = 5;
+		use[x][y] = -1;
+	}else if(use[x][y] == -1){
+		_this.style.backgroundColor = "#2d65fb";
+		use[x][y] = 0;
 	}
+	
 }
 function _disable(ID){
 	var el = document.getElementById(ID);
@@ -160,44 +161,39 @@ function _disable(ID){
 
 function clicked(THIS)
 {
-	var nr = THIS.id;
-	nr = nr.replace("SquareDiv_","");
-	nr = parseInt(nr);
+	var nr = parseInt( THIS.id.substring(10,THIS.id.length) );
+	
 	var x=1,y=1;
-	x = Math.floor((nr-1)/current_cols + 1);
+	x = Math.floor((nr-1)/_cols + 1);
 	
-	y = nr%current_cols;
-	if(y==0) y = current_cols;
-	/*document.getElementById("demo").innerHTML += "mat "+ x+" "+y+" ";*/
+	y = nr%_cols;
+	if(y==0) y = _cols;
 	
-	if( stare[nr] == 1 ){
-		if( AutoLose ){
+	
+	if( mat[x][y] == -1 ){
+		if( AutoLose == 1 ){
 			for(var i=0; i < bmb.length; ++i){
 				x = bmb[i][0]; y = bmb[i][1];
-				nr = (x-1)*current_cols+y;
+				nr = (x-1)*_cols+y;
 				document.getElementById("SquareDiv_"+nr).style.backgroundColor= "#dd0a2b";
 				_disable("SquareDiv_"+nr);
-				
+				use[x][y] = 1;
 			}
-			
 		}
 		else{		
 			document.getElementById("SquareDiv_"+nr).style.backgroundColor= "#dd0a2b";
 			_disable("SquareDiv_"+nr);
-			mat[x][y] = 103;
+			use[x][y] = 1;
 		}
 	}	
 	else
 	{
-		
-		
-		
-		if(mat[x][y] <0 )
+		if(mat[x][y] >0 )
 		{
-			document.getElementById("SquareDiv_"+nr).innerText = -mat[x][y];
+			document.getElementById("SquareDiv_"+nr).innerText = mat[x][y];
 			document.getElementById("SquareDiv_"+nr).style.backgroundColor="#8ed379";
 			_disable("SquareDiv_"+nr);
-			mat[x][y] = 100;
+			use[x][y] = 2;
 		}
 		
 		else if(mat[x][y]==0){
@@ -212,28 +208,30 @@ function fill_(x,y){
 	y = parseInt(y);
 	
 	var nr = 0;
-	var stx = [x];
 	
+	var stx = [x];
 	var sty = [y];
 	var index = 0;
+	
 	var i=0,j=0;
+	
 	for( ; index < stx.length ; ++index )
 	{
 		x = stx[index];
 		y = sty[index];
 		for(var a=-1;a<2;++a){
 			i = x+a; 
-			if( i <1 || i>current_rows) continue;
+			if( i <1 || i>_rows) continue;
 			for(var b=-1;b<2;++b){
 				j=y+b;
 				if(a==0 && b == 0) continue;
-				if( j <1 || j > current_cols ) continue;
-				if(mat[i][j] == 0){
+				if( j <1 || j > _cols ) continue;
+				if(mat[i][j] == 0 && use[i][j] ==0){
 					stx.length = stx.length+1;
 					stx[stx.length-1] = i;
 					sty.length = sty.length+1;
 					sty[sty.length-1] = j;
-					mat[i][j]= 104;
+					use[i][j] = 2;
 				}
 			}
 		}
@@ -243,23 +241,23 @@ function fill_(x,y){
 	{
 		x = stx[index];
 		y = sty[index];
-		nr = (x-1)*current_rows + y; 
+		nr = (x-1)*_cols + y; 
 		document.getElementById("SquareDiv_"+nr).style.backgroundColor="#8ed379";
 		_disable("SquareDiv_"+nr);
 		
 		for(var a=-1;a<2;++a){
 			i = x+a; 
-			if( i <1 || i>current_rows) continue;
+			if( i <1 || i > _rows) continue;
 			for(var b=-1;b<2;++b){
 				j=y+b;
 				if(a==0 && b == 0) continue;
-				if( j <1 || j > current_cols ) continue;
-				if(mat[i][j] < 0){
-					nr = (i-1)*current_rows + j; 
-					document.getElementById("SquareDiv_"+nr).innerText = -mat[i][j];
+				if( j <1 || j > _cols ) continue;
+				if(mat[i][j] > 0){
+					nr = (i-1)*_cols + j; 
+					document.getElementById("SquareDiv_"+nr).innerText = mat[i][j];
 					document.getElementById("SquareDiv_"+nr).style.backgroundColor="#8ed379";
 					_disable("SquareDiv_"+nr);
-					mat[i][j] = 102;
+					use[i][j] = 2;
 				}
 			}
 		}
@@ -293,18 +291,18 @@ function _LoadSquares(rows,cols)
 
 }
 
-function m_on(_this){
-	var id = _this.id;
-	id = id.replace("SquareDiv_","");
-	id = parseInt(id);
-	if(stare[id] == 5 ) return;
-	_this.style.backgroundColor = "#0f2e83";
+function m_on(THIS){
+	var nr = parseInt( THIS.id.substring(10,THIS.id.length) );
+	var x,y;
+	x=cx[nr] ; y =cy[nr];
+	if(use[x][y] != 0 ) return;
+	THIS.style.backgroundColor = "#0f2e83";
 }
 
-function m_out(_this){
-	var id = _this.id;
-	id = id.replace("SquareDiv_","");
-	id = parseInt(id);
-	if(stare[id] == 5 ) return;
-	_this.style.backgroundColor = "#2d65fb";
+function m_out(THIS){
+	var nr = parseInt( THIS.id.substring(10,THIS.id.length) );
+	var x,y;
+	x=cx[nr] ; y =cy[nr];
+	if(use[x][y] != 0 ) return;
+	THIS.style.backgroundColor = "#2d65fb";
 }
